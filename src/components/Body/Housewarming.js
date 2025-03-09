3e1984097664cc79dd70e635efa41197a05e0867
LBOMI <lcil2293@naver.com>
2025-03-09 11:05:07 +0900

    complete, 스타일 검토

--- src/components/Body/Housewarming.js
+++ src/components/Body/Housewarming.js
@@ -1,200 +1,68 @@
 import styled from "styled-components";
 
+import ScrapButton from "../ScrapButton";
+
 function Housewarming() {
     return (
         <Container>
             <Header>
               <HeaderOne>
-                {/* <div className='B_lookfor2_1_1'> */}
                   <strong>이주의 집들이! 발굴하러 가볼까요?</strong>
-                {/* </div> */}
               </HeaderOne>
-              <div className='B_lookfor_h2'>
+              <HeaderTwo>
                 <button type='button'>더보기</button>
-              </div>
+              </HeaderTwo>
             </Header>
-            <div className='B_lookfor2_2'>
-              <div className='B_lookfor2_2_1'>
-                <div className='B_lookfor2_2_1_1'>
-                  <div className='B_lookfor2_2_1_1_1'>
-                   
+              <Body>
+                <Contents>
+                  <Img>
                     <img src='jipdlei\집들이1.avif' />
-                    
-                    <div className='B_lookfor2_2_1_1_1_1'>
-                      <div className='B_lookfor2_2_1_1_1_1_1'></div>
-                    </div>
-                    <div className='B_lookfor2_2_1_1_1_2'>
-                    <button type='button' >
-                              <span>
-                                <svg style={{width: 24, height: 24, fill:'none'}}>
-                                  <path className='B_lookfor2_2_1_1_1_2_path'></path>
-                                  </svg>
-                                <svg style={{width: 24, height: 24, fill:'none', opacity:1}}>
-                                  <g >
-                                    <path className='B_lookfor2_2_1_1_1_2_path2'></path>
-                                    <path className='B_lookfor2_2_1_1_1_2_path3'></path>
-                                  </g>
-                                  <defs>
-                                    <clipPath>
-                                      <path className='B_lookfor2_2_1_1_1_2_path4'></path>
-                                    </clipPath>
-                                    <filter>
-                                      <feFlood style={{floodOpacity:0}}></feFlood>
-                                      <feColorMatrix style={{values: "0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"}}></feColorMatrix>
-                                      <feOffset style={{dy: 2}}></feOffset>
-                                      <feGaussianBlur style={{stdDeviation: 2.5}}></feGaussianBlur>
-                                      <feColorMatrix style={{values:"0 0 0 0 0.247059 0 0 0 0 0.278431 0 0 0 0 0.301961 0 0 0 0.15 0"}}></feColorMatrix>
-                                      <feBlend></feBlend>
-                                      <feBlend></feBlend>
-                                    </filter>
-                                  </defs>
-                                  </svg>
-                              </span>
-                            </button>
-                    </div>
-                  </div>
-                  <div className='B_lookfor2_2_1_1_2'>
+                    <StyledScrapButton/>
+                  </Img>
+                  <Text>
                     <p>
                       <span>식물과 컬러로 포인트! </span>
                       공간 디자이너 부부와 아이가 사는 집
                     </p>
-                  </div>
-                  <a></a>
-                </div>
-                <div className='B_lookfor2_2_1_1'>
-                  <div className='B_lookfor2_2_1_1_1'>
-                    <img src='jipdlei\집들이2.avif' style={{height:179}}></img>
-                    <div className='B_lookfor2_2_1_1_1_1'>
-                      <div className='B_lookfor2_2_1_1_1_1_1'></div>
-                    </div>
-                    <div className='B_lookfor2_2_1_1_1_2'>
-                    <button type='button' >
-                              <span>
-                                <svg style={{width: 24, height: 24, fill:'none'}}>
-                                  <path className='B_lookfor2_2_1_1_1_2_path'></path>
-                                  </svg>
-                                <svg style={{width: 24, height: 24, fill:'none', opacity:1}}>
-                                  <g >
-                                    <path className='B_lookfor2_2_1_1_1_2_path2'></path>
-                                    <path className='B_lookfor2_2_1_1_1_2_path3'></path>
-                                  </g>
-                                  <defs>
-                                    <clipPath>
-                                      <path className='B_lookfor2_2_1_1_1_2_path4'></path>
-                                    </clipPath>
-                                    <filter>
-                                      <feFlood style={{floodOpacity:0}}></feFlood>
-                                      <feColorMatrix style={{values: "0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"}}></feColorMatrix>
-                                      <feOffset style={{dy: 2}}></feOffset>
-                                      <feGaussianBlur style={{stdDeviation: 2.5}}></feGaussianBlur>
-                                      <feColorMatrix style={{values:"0 0 0 0 0.247059 0 0 0 0 0.278431 0 0 0 0 0.301961 0 0 0 0.15 0"}}></feColorMatrix>
-                                      <feBlend></feBlend>
-                                      <feBlend></feBlend>
-                                    </filter>
-                                  </defs>
-                                  </svg>
-                              </span>
-                            </button>
-                    </div>
-                  </div>
-                  <div className='B_lookfor2_2_1_1_2'>
+                  </Text>
+                </Contents>
+                <Contents>
+                  <Img>
+                    <img src='jipdlei\집들이2.avif'/>
+                    <StyledScrapButton/>
+                  </Img>
+                  <Text>
                     <p>
                       <span>블랙&그레이톤으로 </span>
                       정돈된 호텔의 무드를 느낄 수 있는 집
                     </p>
-                  </div>
-                  <a></a>
-                </div>
-                <div className='B_lookfor2_2_1_1'>
-                  <div className='B_lookfor2_2_1_1_1'>
-                    <img src='jipdlei\집들이3.webp' style={{width: 268 ,height:179}}></img>
-                    <div className='B_lookfor2_2_1_1_1_1'>
-                      <div className='B_lookfor2_2_1_1_1_1_1'></div>
-                    </div>
-                    <div className='B_lookfor2_2_1_1_1_2'>
-                    <button type='button' >
-                              <span>
-                                <svg style={{width: 24, height: 24, fill:'none'}}>
-                                  <path className='B_lookfor2_2_1_1_1_2_path'></path>
-                                  </svg>
-                                <svg style={{width: 24, height: 24, fill:'none', opacity:1}}>
-                                  <g >
-                                    <path className='B_lookfor2_2_1_1_1_2_path2'></path>
-                                    <path className='B_lookfor2_2_1_1_1_2_path3'></path>
-                                  </g>
-                                  <defs>
-                                    <clipPath>
-                                      <path className='B_lookfor2_2_1_1_1_2_path4'></path>
-                                    </clipPath>
-                                    <filter>
-                                      <feFlood style={{floodOpacity:0}}></feFlood>
-                                      <feColorMatrix style={{values: "0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"}}></feColorMatrix>
-                                      <feOffset style={{dy: 2}}></feOffset>
-                                      <feGaussianBlur style={{stdDeviation: 2.5}}></feGaussianBlur>
-                                      <feColorMatrix style={{values:"0 0 0 0 0.247059 0 0 0 0 0.278431 0 0 0 0 0.301961 0 0 0 0.15 0"}}></feColorMatrix>
-                                      <feBlend></feBlend>
-                                      <feBlend></feBlend>
-                                    </filter>
-                                  </defs>
-                                  </svg>
-                              </span>
-                            </button>
-                    </div>
-                  </div>
-                  <div className='B_lookfor2_2_1_1_2'>
+                  </Text>
+                </Contents>
+                <Contents>
+                  <Img>
+                    <img src='jipdlei\집들이3.webp'/>
+                    <StyledScrapButton/>
+                  </Img>
+                  <Text>
                     <p>
                       <span>공간 분위기 바꾸기? </span>
                       가구&소품 재배치로 어렵지 않게 완성!
                     </p>
-                  </div>
-                  <a></a>
-                </div>
-                <div className='B_lookfor2_2_1_1'>
-                  <div className='B_lookfor2_2_1_1_1'>
-                    <img src='jipdlei\집들이4.avif' style={{height:179}}></img>
-                    <div className='B_lookfor2_2_1_1_1_1'>
-                      <div className='B_lookfor2_2_1_1_1_1_1'></div>
-                    </div>
-                    <div className='B_lookfor2_2_1_1_1_2'>
-                    <button type='button' >
-                              <span>
-                                <svg style={{width: 24, height: 24, fill:'none'}}>
-                                  <path className='B_lookfor2_2_1_1_1_2_path'></path>
-                                  </svg>
-                                <svg style={{width: 24, height: 24, fill:'none', opacity:1}}>
-                                  <g >
-                                    <path className='B_lookfor2_2_1_1_1_2_path2'></path>
-                                    <path className='B_lookfor2_2_1_1_1_2_path3'></path>
-                                  </g>
-                                  <defs>
-                                    <clipPath>
-                                      <path className='B_lookfor2_2_1_1_1_2_path4'></path>
-                                    </clipPath>
-                                    <filter>
-                                      <feFlood style={{floodOpacity:0}}></feFlood>
-                                      <feColorMatrix style={{values: "0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"}}></feColorMatrix>
-                                      <feOffset style={{dy: 2}}></feOffset>
-                                      <feGaussianBlur style={{stdDeviation: 2.5}}></feGaussianBlur>
-                                      <feColorMatrix style={{values:"0 0 0 0 0.247059 0 0 0 0 0.278431 0 0 0 0 0.301961 0 0 0 0.15 0"}}></feColorMatrix>
-                                      <feBlend></feBlend>
-                                      <feBlend></feBlend>
-                                    </filter>
-                                  </defs>
-                                  </svg>
-                              </span>
-                            </button>
-                    </div>
-                  </div>
-                  <div className='B_lookfor2_2_1_1_2'>
+                  </Text>
+                </Contents>
+                <Contents>
+                  <Img>
+                    <img src='jipdlei\집들이4.avif'></img>
+                    <StyledScrapButton/>
+                  </Img>
+                  <Text>
                     <p>
                       <span>30평대 같은 </span>
                       대면형 주방을 실현한, 18평 신희타 리모델링기
                     </p>
-                  </div>
-                  <a></a>
-                </div>
-              </div>
-            </div>
+                  </Text>
+                </Contents>
+              </Body>
             </Container>
 
     )
@@ -243,4 +111,107 @@ const HeaderOne = styled.div`
     }
   }
 `
+
+const HeaderTwo = styled.div`
+  display: flex;
+  -webkit-box-align: center;
+  align-items: center;
+  height: 0px;
+  margin-left: 10px;
+
+  button {
+    display: inline-block;
+    margin: 0px;
+  padding: 0px;
+  border: none;
+  background: none;
+  font-style: inherit;
+  font-variant: inherit;
+  font-stretch: inherit;
+  font-family: inherit;
+  font-optical-sizing: inherit;
+  font-kerning: inherit;
+  font-feature-settings: inherit;
+  font-variation-settings: inherit;
+  font-size: 16px;
+  line-height: 20px;
+  font-weight: 700;
+  color: rgb(53, 197, 240);
+  cursor: pointer;
+  
+  &:hover {
+    color: rgb(165, 210, 224);
+  }
+  }
+`
+
+const Body = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 20px;
+`
+
+const Contents = styled.div`
+  position: relative;
+  display: flex;
+  width: 100%;
+  min-width: 0px;
+  flex-flow: column;
+`
+
+const Img = styled.div`
+  height: 179px;
+  overflow: hidden;
+  position: relative;
+  border-radius: 4px;
+  display: flex;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    top: 0px;
+    left: 0px;
+    transform: scale(1);
+    transition: transform 0.2s ease 0s;
+
+    &:hover {
+        transform: scale(1.05);
+        transition: transform .25s;
+      } 
+  }
+
+  
+`
+
+const StyledScrapButton = styled(ScrapButton)`
+  position: absolute;  // 이미지 위에 배치
+  bottom: 10px;        
+  right: 5px;
+  border-radius: 50%;
+  padding: 8px;
+  // z-index: 10000;   
+`;
+
+const Text = styled.div`
+  margin-top: 5px;
+  min-width: 0px;
+  font-size: 16px;
+  line-height: 20px;
+  font-weight: 400;
+  color: rgb(47, 52, 56);
+  word-break: keep-all;
+  overflow-wrap: break-word;
+  cursor: pointer;
+
+  p {
+    overflow: hidden;
+
+    span {
+      color: rgb(53, 197, 240);
+      font-weight: 700;
+    }
+  }
+`
+
 export default Housewarming;
