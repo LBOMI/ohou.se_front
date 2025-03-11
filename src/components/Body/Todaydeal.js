5c5cccba16d0dad30a2916f61389c458eeb38d70
LBOMI <lcil2293@naver.com>
2025-03-11 14:40:30 +0900

    오늘의 딜 컴포넌트화

---
+++ src/components/Body/Todaydeal.js
@@ -0,0 +1,100 @@
+import styled from "styled-components";
+
+import Todaydeal_Slider from "../Slide/TodaydealSlider";
+
+function Todaydeal() {
+    return (
+        <Container>
+            <Header>
+              <HeaderOne>
+                  <strong>오늘의 딜</strong>
+              </HeaderOne>
+              <HeaderTwo>
+                <button type='button'>더보기</button>
+              </HeaderTwo>
+            </Header>
+
+            <Todaydeal_Slider/>
+            
+          </Container>
+    )
+}
+
+const Container = styled.div`
+  // width: 100%;
+  box-sizing: border-box;
+  background: rgb(255, 255, 255);
+  max-width: 1256px;
+  padding: 50px 60px 10px 60px;
+  margin: auto;
+`
+
+const Header = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  padding: 20px 0px;
+  font-size: 0px;
+  line-height: 1;
+`
+
+const HeaderOne = styled.div`
+  flex: 1 1 0%;
+  display: flex;
+  align-items: flex-start;
+  flex-direction: column;
+  // min-width: 0px;
+  cursor: pointer;
+
+  strong {
+    overflow: hidden;
+    display: inline-block;
+    font-weight: 700;
+    color: rgb(47, 52, 56);
+    letter-spacing: -0.3px;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    font-size: 20px;
+    line-height: 28px;
+
+    &:hover {
+      color: rgb(136, 141, 144);
+    }
+  }
+`
+
+const HeaderTwo = styled.div`
+  display: flex;
+  -webkit-box-align: center;
+  align-items: center;
+  height: 0px;
+  margin-left: 10px;
+
+  button {
+    display: inline-block;
+    margin: 0px;
+  padding: 0px;
+  border: none;
+  background: none;
+  font-style: inherit;
+  font-variant: inherit;
+  font-stretch: inherit;
+  font-family: inherit;
+  font-optical-sizing: inherit;
+  font-kerning: inherit;
+  font-feature-settings: inherit;
+  font-variation-settings: inherit;
+  font-size: 16px;
+  line-height: 20px;
+  font-weight: 700;
+  color: rgb(53, 197, 240);
+  cursor: pointer;
+  
+  &:hover {
+    color: rgb(165, 210, 224);
+  }
+  }
+`
+
+export default Todaydeal;
