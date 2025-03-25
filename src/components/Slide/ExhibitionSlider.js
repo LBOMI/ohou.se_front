8ac659c9c632bfe15cd9a4f8fe65cab2ec76b108
LBOMI <lcil2293@naver.com>
2025-03-25 13:47:23 +0900

    더보기, 반응형 슬라이드 추가

--- src/components/Slide/ExhibitionSlider.js
+++ src/components/Slide/ExhibitionSlider.js
@@ -2,8 +2,6 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {faChevronRight} from '@fortawesome/free-solid-svg-icons'
-import {faChevronLeft} from '@fortawesome/free-solid-svg-icons'
 import {faArrowRight} from '@fortawesome/free-solid-svg-icons'
 
 import PrevArrow from './PrevArrow';
@@ -11,11 +9,10 @@ import NextArrow from './NextArrow';
 
 import styled from "styled-components";
 
-import { useState, useRef } from 'react';
+import { useState } from 'react';
 
 
 function Exhibition_Slider() {
-    const sliderRef = useRef(null);
     const [currentSlide, setCurrentSlide] = useState(0);
   
     const settings = {
@@ -27,165 +24,68 @@ function Exhibition_Slider() {
       slidesToScroll: 4,
       afterChange: (current) => setCurrentSlide(current),
       prevArrow: <PrevArrow/>,
-      nextArrow: <NextArrow/>
+      nextArrow: <NextArrow/>,
+
+      responsive : [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
     };
 
     const categories = [
-      { img: 'exhibition/기획전_1.avif', p1: '전시, 반품, 스크래치 특템의 기회!', p2: '빠른 품절! 리퍼마켓 ~82%'},
+      { img: 'exhibition/기획전_1.avif', p1: '전시, 반품, 스크래치 득템의 기회!', p2: '빠른 품절! 리퍼마켓 ~82%'},
       { img: 'exhibition/기획전_2.avif', p1: '전상품 무료배송', p2: '매일 새로운 장보기 핫딜! 오마트'},
       { img: 'exhibition/기획전_3.avif', p1: '오늘의집 Only'},
       { img: 'exhibition/기획전_4.avif', p1: '배송 걱정없이 원하는 날 한번에 설치해요!', p2: '오늘의집 배송 꿀조합 가구 추천'},
-      { img: 'exhibition/기획전_2.avif', p1: '전상품 무료배송', p2: '매일 새로운 장보기 핫딜! 오마트'}
+      { isMore: true } // 더보기
 
     ]
   
     return (
       <SliderContainer>
-        <StyledSlider ref={sliderRef} {...settings} >
+        <StyledSlider {...settings} >
        
         
-          {categories.map(({ img, p1, p2 }, idx ) => (
-            <Container key = {img}>
-              <Image>
-                <img src={img} />
-              </Image>
+          {categories.map(({ img, p1, p2, isMore }, idx ) => (
+            isMore ? (
+              <Container key = "more" >
+                <MoreContainer>
+                  <a>
+                  <Arrow>
+                    <span><FontAwesomeIcon icon={faArrowRight} /></span>
+                  </Arrow>
+                  <Plus>더보기</Plus>
+                  </a>
+                </MoreContainer>
+              </Container>
+            ) : (
+              <Container key = { `slide-${idx}`}>
+                <Image>
+                  <img src={img} alt={p1 || '기획전 이미지'}/>
+                </Image>
               <Text>
                 <P1>{p1}</P1>
                 <P2>{p2}</P2>
               </Text>
 
             </Container>
-          ))}
-                        
-                    
-                    {/* <div className='Exhibition_c1_1_2'>
-                        <div className='Exhibition_c1_1_2_1' style={{cursor:"pointer"}}>
-                        <div className='Exhibition_c1_1_1_174' style={{ marginRight: 20 }}>
-                          <img src='exhibition\기획전_2.avif' ></img>
-                          <div className='Exhibition_c1_1_2_1_1'>
-                            <div className='Exhibition_c1_1_2_1_1_1'></div>
-                          </div>
-                          </div>
-                        </div>
-                        <div className='Exhibition_c1_1_1_2' style={{cursor:"pointer"}}>
-                          <p className='Exhibition_c1_1_1_2_p1'>전상품 무료배송</p>
-                          <p className='Exhibition_c1_1_1_2_p2'>매일 새로운 장보기 핫딜! 오마트</p>
-                        </div>
-                        <a></a>
-                      </div>
-                      <div className='Exhibition_c1_1_3'>
-                        <div className='Exhibition_c1_1_3_1' style={{cursor:"pointer"}}>
-                        <div className='Exhibition_c1_1_1_174' style={{ marginRight: 20 }}>
-                          <img src='exhibition\기획전_3.avif'></img>
-                          <div className='Exhibition_c1_1_3_1_1'>
-                            <div className='Exhibition_c1_1_3_1_1_1'></div>
-                          </div>
-                          </div>
-  
-                        </div>
-                        <div className='Exhibition_c1_1_3_2' style={{cursor:"pointer"}}>
-                          <p className='Exhibition_c1_1_3_2_p1'></p>
-                          <p className='Exhibition_c1_1_3_2_p2'>오늘의집 Only</p>
-                        </div>
-                        <a></a>
-                      </div>
-                      <div className='Exhibition_c1_1_4'>
-                        <div className='Exhibition_c1_1_4_1' style={{cursor:"pointer"}}>
-                        <div className='Exhibition_c1_1_1_174' style={{ marginRight: 20 }}>
-                          <img src='exhibition\기획전_4.avif'></img>
-                          <div className='Exhibition_c1_1_4_1_1'>
-                            <div className='Exhibition_c1_1_4_1_1_1'></div>
-                          </div>
-                          </div>
-  
-                        </div>
-                        <div className='Exhibition_c1_1_4_2' style={{cursor:"pointer"}}>
-                          <p className='Exhibition_c1_1_4_2_p1'>배송 걱정없이 원하는 날 한번에 설치해요!</p>
-                          <p className='Exhibition_c1_1_4_2_p2'>오늘의집 배송 꿀조합 가구 추천</p>
-                        </div>
-                        <a></a>
-                      </div> */}
-                      <div>
-                      <div className='Exhibition_c1_1_5'>
-                      <a>
-                        <div className='Exhibition_c1_1_5_1' >
-                          <span><FontAwesomeIcon icon={faArrowRight} /></span>
-                        </div>
-                        <div className='Exhibition_c1_1_5_2'>더보기</div>
-                      </a>
-                      </div>
-                      </div>
-  
-                    
+            )
+            
+          ))}                      
         </StyledSlider>
-        {currentSlide > 0 && (
-        <div  className='PrevArrow2_button'>
-        <button className='PrevArrow2_button_b'
-          onClick={() => sliderRef.current.slickPrev()}
-          style={{
-            margin: 0,
-            border: "none",
-            background: "none",
-            font: "inherit",
-            fontSize: 16,
-            lineHeight: 20,
-            fontWeight: 700,
-            boxSizing: "border-box",
-            textAlign: "center",
-            color: "rgb(255, 255, 255)",
-            padding: 0,
-            width: 48,
-            height: 48,
-            display: "flex",
-            alignItems: "center",
-            webkitboxpack: "center",
-            justifyContent: "center",
-            borderRadius: 24,
-            backgroundColor: "rgb(255, 255, 255)",
-            boxShadow: "0 2px 5px rgba(63, 71, 77, 0.15)",
-            cursor: "pointer",
-            touchAction: "manipulation",
-          }}
-          type='button'
-        >
-        <span><FontAwesomeIcon icon={faChevronLeft} /></span>
-        </button>
-        </div>
-      )}
-      {currentSlide === 0 && (
-        <div  className='NextArrow_button'>
-        <button 
-          onClick={() => sliderRef.current.slickNext()}
-          style={{
-            margin: 0,
-            border: "none",
-            background: "none",
-            font: "inherit",
-            fontSize: 16,
-            lineHeight: 20,
-            fontWeight: 700,
-            boxSizing: "border-box",
-            textAlign: "center",
-            color: "rgb(255, 255, 255)",
-            padding: 0,
-            width: 48,
-            height: 48,
-            display: "flex",
-            alignItems: "center",
-            webkitboxpack: "center",
-            justifyContent: "center",
-            borderRadius: 24,
-            backgroundColor: "rgb(255, 255, 255)",
-            boxShadow: "0 2px 5px rgba(63, 71, 77, 0.15)",
-            cursor: "pointer",
-            touchAction: "manipulation",
-          }}
-          type='button'
-        >
-        <span><FontAwesomeIcon icon={faChevronRight} /></span>
-        </button>
-        </div>
-      )}
+        
       </SliderContainer>
     )
   }
@@ -199,7 +99,7 @@ function Exhibition_Slider() {
     display: flex;
     margin: 0px;
     padding: 0px;
-    overflow: hidden;
+    // overflow: hidden;
     list-style: none;
   `;
 
@@ -255,6 +155,55 @@ function Exhibition_Slider() {
     line-height: 24px;
     font-weight: 700;
     margin: 0;
-  `
+  `;
+
+  const MoreContainer = styled.div`
+    margin-right: 0;
+    display: flex;
+    -webkit-box-align: center;
+    align-items: center;
+    -webkit-box-pack: center;
+    justify-content: center;
+    padding: 0px 20px 0px 0px;
+    padding: 0px;
+    flex-shrink: 0;
+    scroll-snap-align: start;
+    /* width: 266.67px; */
+    height: 213.77px;
+
+    a {
+      color: rgb(47, 52, 56);
+      display: inline-block;
+      font-weight: 500;
+      padding: 6px 16px;
+      cursor: pointer;
+      touch-action: manipulation;
+      text-decoration: none;
+    }
+`;
+  
+const Arrow = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 20px;
+  background-color: rgb(234, 237, 239);
+  margin-bottom: 6px;
+  position: relative;
+
+  span {
+    display: inline-block;
+    font-size: 18px;
+    line-height: 1;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+`;
+
+const Plus = styled.div`
+  font-size: 14px;
+  line-height: 20px;
+`;
 export default Exhibition_Slider;
   
