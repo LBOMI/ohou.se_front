ba7c3df3cd36a12bfb44d4677cfe56cfc9ca830a
LBOMI <lcil2293@naver.com>
2025-03-26 10:37:44 +0900

    슬라이드 최적화, categories 구조 정리, import 순서 정리

--- src/components/Slide/LookforSlider.js
+++ src/components/Slide/LookforSlider.js
@@ -1,13 +1,13 @@
+import React, { useState } from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {faChevronLeft} from '@fortawesome/free-solid-svg-icons'
 import {faArrowRight} from '@fortawesome/free-solid-svg-icons'
 
 import styled from "styled-components";
 
-import { useState, useRef } from 'react';
 import NextArrow from './NextArrow';
 import PrevArrow from './PrevArrow';
 import ScrapButton from '../ScrapButton';
@@ -35,36 +35,37 @@ function LookforSlider() {
         }
       ]
     };
-  
+
+    const defaultMiniImg = 'lookfor_picture/lookfor_mini.avif';
     const categories = [
-      { img1: 'lookfor_picture/lookfor1.avif', img2: 'lookfor_picture/lookfor_mini.avif', userName: 'User1' },
-      { img1: 'lookfor_picture/lookfor2.avif', img2: 'lookfor_picture/lookfor_mini.avif', userName: 'User2' },
-      { img1: 'lookfor_picture/lookfor3.gif', img2: 'lookfor_picture/lookfor_mini.avif', userName: 'User3' },
-      { img1: 'lookfor_picture/lookfor4.avif', img2: 'lookfor_picture/lookfor_mini.avif', userName: 'User4' },
-      { img1: 'lookfor_picture/lookfor5.avif', img2: 'lookfor_picture/lookfor_mini.avif', userName: 'User5' },
-      { img1: 'lookfor_picture/lookfor6.avif', img2: 'lookfor_picture/lookfor_mini.avif', userName: 'User6' },
-      { img1: 'lookfor_picture/lookfor7.avif', img2: 'lookfor_picture/lookfor_mini.avif', userName: 'User7' },
-      { img1: 'lookfor_picture/lookfor8.avif', img2: 'lookfor_picture/lookfor_mini.avif', userName: 'User8' },
+      { img1: 'lookfor_picture/lookfor1.avif', userName: 'User1' },
+      { img1: 'lookfor_picture/lookfor2.avif', userName: 'User2' },
+      { img1: 'lookfor_picture/lookfor3.gif', userName: 'User3' },
+      { img1: 'lookfor_picture/lookfor4.avif', userName: 'User4' },
+      { img1: 'lookfor_picture/lookfor5.avif', userName: 'User5' },
+      { img1: 'lookfor_picture/lookfor6.avif', userName: 'User6' },
+      { img1: 'lookfor_picture/lookfor7.avif', userName: 'User7' },
+      { img1: 'lookfor_picture/lookfor8.avif', userName: 'User8' },
       { isMore: true } 
-    ]
+    ].map(item => ({ ...item, img2: defaultMiniImg}));
     
     return (
       <SliderContainer>
         <StyledSlider {...settings}>
           {categories.map(({ img1, img2, userName, isMore }, idx ) => (
             isMore ? (
-              <div>
-              <div className='B_lookfor_h3_1_2_lastli'>
-                <a>
-                <div className='B_lookfor_h3_1_2_last'>
-                  <span><FontAwesomeIcon icon={faArrowRight} /></span>
-                </div>
-                <div className='B_lookfor_h3_1_2_last2'>더보기</div>
-                </a>
-                </div>
-              </div>
+              <React.Fragment key = {`more-${idx}`}>
+               <MoreContent key={`more-${idx}`}>
+                 <a>
+                  <ArrowRight>
+                   <span><FontAwesomeIcon icon={faArrowRight} /></span>
+                  </ArrowRight>
+                  <MoreText>더보기</MoreText>
+                 </a>
+                </MoreContent>
+              </React.Fragment>
             ) : (
-<Content key = "more">
+              <Content key={`category-${idx}`}>
                         <BackgroundImg>
                           <img src={img1} alt={userName}/>
                         </BackgroundImg>
@@ -79,7 +80,7 @@ function LookforSlider() {
                         <ScrapButtonWrapper>
                           <ScrapButton/>
                         </ScrapButtonWrapper>
-                        <a></a>
+          
                       </Content>
             )
             
@@ -102,7 +103,7 @@ function LookforSlider() {
   const SliderContainer = styled.div`
     position: relative;
     z-index: 0;
-    width: 100%;
+    // width: 100%;
     height: 230.22px;
 
   `;
@@ -118,12 +119,20 @@ function LookforSlider() {
     .slick-track {
       height: 230.22px;
     }
+
+    .slick-slide {
+      padding: 0 8px;
+      box-sizing: border-box;
+    }
+
+    .slick-list {
+      margin: 0 -8px; //마지막 여백 상쇄 처리
+    }
   `;
 
   const Content = styled.div`
     width: 100%;
     height: 100%;
-    margin: 0px 20px 0px 0px;
     padding: 0px;
     flex-shrink: 0;
     scroll-snap-align: start;
@@ -131,8 +140,7 @@ function LookforSlider() {
     position: relative;
     border-radius: 4px;
     display: flex;
-  
-    min-width: 0px;
+
     flex-flow: column;
   `;
 
@@ -144,7 +152,11 @@ function LookforSlider() {
       width: 100%;
       height: 100%;
       object-fit: cover;
-      transition: transform 0.2s ease 0s;
+
+      &:hover {
+        transition: transform 0.2s ease 0s;
+        transform: scale(1.05);
+      }
     }
     
   `;
@@ -152,7 +164,6 @@ function LookforSlider() {
     height: 30px;
     position: absolute;
     padding: 8px;
-    font-size: 0px;
     z-index: 1;
     left: 0px;
     bottom: 0px;
@@ -195,5 +206,53 @@ function LookforSlider() {
     z-index: 1;
     right: 0px;
     bottom: 0px;
+  `;
+
+  const MoreContent = styled.div`
+    display: flex;
+    -webkit-box-align: center;
+    align-items: center;
+    -webkit-box-pack: center;
+    justify-content: center;
+    padding: 0px;
+    flex-shrink: 0;
+    /* scroll-snap-align: start; */
+    width: 172px;
+    height: 230.22px;
+
+    a {
+      color: rgb(47, 52, 56);
+      display: inline-block;
+      font-weight: 500;
+      padding: 6px 16px;
+      cursor: pointer;
+      touch-action: manipulation;
+      width: 50px;
+      height: 80px;
+    }
+  `;
+
+  const ArrowRight = styled.div`
+    width: 40px;
+    height: 40px;
+    border-radius: 20px;
+    background-color: rgb(234, 237, 239);
+    margin-bottom: 6px;
+    position: relative;
+
+    span {
+      display: inline-block;
+      font-size: 18px;
+      line-height: 1;
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+    }
+  `;
+
+  const MoreText = styled.div`
+    font-size: 14px;
+    line-height: 20px;
   `
   export default LookforSlider;
