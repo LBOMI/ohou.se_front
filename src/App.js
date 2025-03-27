5f128f6058e6fe4e4acb7a2ae5ecdf75e71994aa
LBOMI <lcil2293@naver.com>
2025-03-27 17:26:13 +0900

    app.css 제거

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import './App.css';
 
 import Header from './components/Header';
 import Body from './components/Body';
 import Footer from './components/Footer';
+
 
 export default function TodaysHouse() {
   return (
